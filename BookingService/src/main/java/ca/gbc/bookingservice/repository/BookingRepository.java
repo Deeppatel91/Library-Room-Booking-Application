80f0c586b0bea25c468b46f2c2dffb2022bc6b05
Deeppatel91 <deeppatel9171@gmail.com>
2024-11-01 20:09:00 -0400

    Created EventService

--- BookingService/src/main/java/ca/gbc/bookingservice/repository/BookingRepository.java
+++ BookingService/src/main/java/ca/gbc/bookingservice/repository/BookingRepository.java
@@ -12,9 +12,4 @@ public interface BookingRepository extends MongoRepository<Booking, String> {
     @Query("{'roomId': ?0, $or: [{'startTime': {$lt: ?2, $gte: ?1}}, {'endTime': {$lte: ?2, $gt: ?1}}, {'startTime': {$lte: ?1}, 'endTime': {$gte: ?2}}]}")
     List<Booking> findConflictingBookings(String roomId, LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query("{ 'userId': ?0, 'roomId': ?1, 'startTime': { $lt: ?3 }, 'endTime': { $gt: ?2 } }")
-    List<Booking> findConflictingBookingsByUser(String userId, String roomId, LocalDateTime startTime, LocalDateTime endTime);
-
-    @Query("{ 'roomId': ?0, 'startTime': { $lt: ?2 }, 'endTime': { $gt: ?1 } }")
-    List<Booking> findConflictingBookingsForRoom(String roomId, LocalDateTime startTime, LocalDateTime endTime);
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "microservice-parent"
 include("RoomService")
 include("UserService")
 include("BookingService")
+include("EventService")
